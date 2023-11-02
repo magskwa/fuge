a9e1d6ed49265d9d10e129bafb86453869c5fdc5
Magali Egger <magali.egger@heig-vd.ch>
2023-11-02 14:32:42 +0100

    add cmakelists.txt

--- test_fuge.py
+++ test_fuge.py
@@ -5,6 +5,7 @@ import os
 path_to_fuge_4 = "/Users/mer/Documents/fuge/build-FUGE-LC-Qt_macoOS_v4-Debug/FUGE-LC.app/Contents/MacOS/FUGE-LC"
 path_to_fuge_5 = "/Users/mer/Documents/fuge/build-FUGE-LC-Qt_macOS_v5-Debug/FUGE-LC.app/Contents/MacOS/FUGE-LC"
 path_to_fuge_6 ="/Users/mer/Documents/fuge/build-FUGE-LC-Qt_6_5_3_for_macOS-Debug/FUGE-LC.app/Contents/MacOS/FUGE-LC"
+path_to_fuge_6_cmake = "/Users/mer/Documents/fuge/fuge-lc/cmake-build-debug/FUGE-LC.app/Contents/MacOS/FUGE-LC"
 path_to_dataset_arr = "/Users/mer/Documents/projet/dataset/arrhythmia_binary_sorted.csv"
 path_to_script_arr = "/Users/mer/Documents/projet/dataset/test.fs"
 path_to_dataset_can = "/Users/mer/Documents/projet/dataset/CancerDiag2_headers.csv"
@@ -12,7 +13,7 @@ path_to_script_can = "/Users/mer/Documents/projet/dataset/CancerRef.fs"
 path_to_script_result = "/Users/mer/Desktop/script_result/"
 
 # Run the fuge app to create the fuzzy systems
-os.system(path_to_fuge_6 + " -d " + path_to_dataset_arr + " -s " + path_to_script_arr + " -g no" + '\n')
+os.system(path_to_fuge_6_cmake + " -d " + path_to_dataset_arr + " -s " + path_to_script_arr + " -g no" + '\n')
 
 
 # wait for the fuzzy systems to be created
@@ -34,7 +35,7 @@ if not os.path.exists(path_to_script_result + "evaluation"):
 # Run the fuge app to evaluate the fuzzy systems
 for fuzzy_sys_name in fuzzy_sys_names:
     path_to_result = path_to_script_result + "evaluation/" + fuzzy_sys_name
-    os.system(path_to_fuge_6 + " --evaluate " + " -d " + path_to_dataset_arr + " -s " + path_to_script_arr + " -f " + path_to_fuzzy_systems + fuzzy_sys_name + " -g no > " + path_to_result)
+    os.system(path_to_fuge_6_cmake + " --evaluate " + " -d " + path_to_dataset_arr + " -s " + path_to_script_arr + " -f " + path_to_fuzzy_systems + fuzzy_sys_name + " -g no > " + path_to_result)
 
  
 # read the result from result file, find the fitness and print it
